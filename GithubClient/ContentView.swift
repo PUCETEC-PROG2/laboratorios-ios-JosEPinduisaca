d1c08d1d0b5f1ff4285d6f39a32ca19a97f6792a
Usuario invitado <Guest@iMac-de-PUCE-A10810.local>
2026-07-11 10:43:41 -0500

    Primer commit

---
+++ GithubClient/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  GithubClient
+//
+//  Created by Usuario invitado on 13/1/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            RepoList()
+                .tabItem{
+                    Label("Repositorios",
+                          systemImage:
+                            "arrow.trianglehead.branch")
+                }
+            RepoForm()
+                .tabItem{ Label("Crear repositorio",
+                                systemImage: "plus")
+                }
+            Profile()
+                .tabItem{
+                    Label("Perfil de usuario",
+                    systemImage: "person.crop.circle")
+                }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
